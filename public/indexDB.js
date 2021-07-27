a76222a48374b25d1998cf0f07530fb7067a1a03
Joel Martinez <joemartinez87@hotmail.com>
2021-07-26 19:33:23 -0700

    creater a service worker file, manifest file, and indexDB file

---
+++ public/indexDB.js
@@ -0,0 +1,76 @@
+let db;
+let budgetVersion;
+
+
+const request = indexedDB.open('Budget_TrackerDB', budgetVersion || 21);
+
+request.onupgradeneeded = function (e) {
+  console.log('Upgrade needed in IndexDB');
+
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
+
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
+
+  db = e.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('TrackerStore', { autoIncrement: true });
+  }
+};
+
+request.onerror = function (e) {
+  console.log(`Woops! ${e.target.errorCode}`);
+};
+
+function checkDatabase() {
+  console.log('check db invoked');
+
+  let transaction = db.transaction(['TrackerStore'], 'readwrite');
+
+  const store = transaction.objectStore('TrackerStore');
+
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          if (res.length !== 0) {
+            transaction = db.transaction(['TrackerStore'], 'readwrite');
+
+            const currentStore = transaction.objectStore('TrackerStore');
+            currentStore.clear();
+            console.log('Clearing store 🧹');
+          }
+        });
+    }
+  };
+}
+
+request.onsuccess = function (e) {
+  console.log('success');
+  db = e.target.result;
+  if (navigator.onLine) {
+    console.log('Backend online! 🗄️');
+    checkDatabase();
+  }
+};
+
+const saveRecord = (record) => {
+  console.log('Save record invoked');
+  const transaction = db.transaction(['TrackerStore'], 'readwrite');
+  const store = transaction.objectStore('TrackerStore');
+
+  store.add(record);
+};
+
+window.addEventListener('online', checkDatabase);
