71367d2621003968e5389cbe4df2ef7f318b6a15
Joel Martinez <joemartinez87@hotmail.com>
2021-07-30 21:03:33 -0700

    fix: refactor hard coded connection string to Heroku config var

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const uri = process.env.MONGODB_URI;
 
 const PORT = 3001;
 
@@ -15,12 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
